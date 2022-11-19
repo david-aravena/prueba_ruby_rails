25e0bfa3ed6d24f9c077a6bf035e1dfbe5a5fb4a
david <david.aravenap@gmail.com>
2022-11-19 02:28:27 -0300

    crud properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,16 +1,43 @@
 class PropertiesController < ApplicationController
   def index
+    @properties = Property.all
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    @property.user_id = current_user.id
+    if @property.save
+      flash[:notice] = 'Product added!'   
+      redirect_to root_path   
+    else
+      flash[:error] = 'Failed to edit product!'   
+      render :new
+    end
   end
 
   def edit
+    @property = Property.find(params[:id])
   end
 
   def update
+    @property = Property.find(params[:id])
+    @property.update(property_params)
+    redirect_to root_path 
   end
 
   def destroy
+    @property = Property.find(params[:id])
+    @property.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:price, :address, :area, :rooms, :toilets, :photo, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'properties/new'
-  get 'properties/edit'
-  get 'properties/update'
-  get 'properties/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "properties#index"
+  get 'properties/new', to: 'properties#new', as: "new_property"
+  post 'properties', to: 'properties#create'
+  patch 'properties/update/:id', to: 'properties#update', as:"update_property" 
+
+  
+
+  get 'properties/edit/:id', to: 'properties#edit', as: 'edit_property'
+  get 'properties/delete/:id', to: 'properties#destroy', as: 'delete_property'
 end
